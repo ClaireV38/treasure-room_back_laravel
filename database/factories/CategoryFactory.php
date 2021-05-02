dcd67b4fed56cee0d7c08b834cb6737dd5d38313
ClaireV38 <claireverdier00@gmail.com>
2021-05-02 17:05:18 +0200

    create seeders for category and users

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -2,21 +2,21 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use phpDocumentor\Reflection\Types\Integer;
 
-class UserFactory extends Factory
+class CategoryFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = User::class;
+    protected $model = Category::class;
 
-    const ADVENTURERS = ['Jason','Ulysse','Helene','Salakis','Heracles','Achille','Egée','Oedipe','Persée','Sirtakis'];
+    const CATEGORIES = ['Pierres précieuses', 'Sculptures', 'Pièces d\'or'];
 
     /**
      * Define the model's default state.
@@ -26,11 +26,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->unique()->randomElement(self::ADVENTURERS),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'name' => $this->faker->unique()->randomElement(self::CATEGORIES),
         ];
     }
 
